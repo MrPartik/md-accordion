24eb2ec12caaa9228b7987a43eedf85c7f13a3e9
zakardo <abderrezak.djadoun@seerene.com>
2016-09-25 23:12:21 +0200

    renamed css classes properly

--- material-accordion.js
+++ material-accordion.js
@@ -21,7 +21,7 @@ function mdAccordion($timeout) {
 	};
 
 	function link(scope, element, attrs) {
-		element.find(".md-accordion-item-body").addClass('element-hidden');
+		element.find(".md-accordion-item-body").addClass('item-closed');
 		var elem = element.find('.md-accordion-item-body');
 		element.find('.md-accordion-item-header').on('click', function(){
 			// element.find('.md-accordion-item-body').addClass('hide');
@@ -78,25 +78,25 @@ function mdAccordionItemHeader($timeout) {
 		console.log(isOne)
 		element.on('click', function(){
 			if(isOne != undefined){
-				if(element.parent().find('.md-accordion-item-body').hasClass('shown-element')){
+				if(element.parent().find('.md-accordion-item-body').hasClass('item-open')){
 					console.debug("has class")
-					element.parent().find('.md-accordion-item-body').addClass('element-hidden');	
-					element.parent().find('.md-accordion-item-body').removeClass('shown-element');	
+					element.parent().find('.md-accordion-item-body').addClass('item-closed');	
+					element.parent().find('.md-accordion-item-body').removeClass('item-open');	
 
 				}else{
-					element.parent().parent().find('.md-accordion-item-body').removeClass('shown-element');
-					element.parent().parent().find('.md-accordion-item-body').addClass('element-hidden');
+					element.parent().parent().find('.md-accordion-item-body').removeClass('item-open');
+					element.parent().parent().find('.md-accordion-item-body').addClass('item-closed');
 					$timeout(function(){
-						element.parent().find('.md-accordion-item-body').toggleClass('element-hidden');
-						element.parent().find('.md-accordion-item-body').toggleClass('shown-element');
+						element.parent().find('.md-accordion-item-body').toggleClass('item-closed');
+						element.parent().find('.md-accordion-item-body').toggleClass('item-open');
 					}, 100);
 					
 				}
 			}else{
 				console.log(" not item heder directly")
 
-				element.parent().find('.md-accordion-item-body').toggleClass('element-hidden');
-				element.parent().find('.md-accordion-item-body').toggleClass('shown-element');
+				element.parent().find('.md-accordion-item-body').toggleClass('item-closed');
+				element.parent().find('.md-accordion-item-body').toggleClass('item-open');
 			}
 
 		})
